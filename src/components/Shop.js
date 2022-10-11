23751fa7b61025dc6b099496e3da4ae2199ee36b
Muhammad Rashid <rashidkaif1@yahoo.com>
2022-10-11 15:10:03 +0500

    just setup app files

---
+++ src/components/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    
+    <div>
+        <h2>Buy Addidas shoes 50 rupees</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Add this item to createRoo
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+
+  )
+}
+
+export default Shop
